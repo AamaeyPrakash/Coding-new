3501e8a2aff6673d5213866208b31ab49ad1f23d
AamaeyPrakash <aamaeyprakash@gmail.com>
2024-06-18 11:57:49 -0400

    responsive slider + arrows

--- Disney Clone/disney-clone/src/Components/Slider.jsx
+++ Disney Clone/disney-clone/src/Components/Slider.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import GlobalAPi from '../Services/GlobalApi.jsx'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 
 const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+const screenWidth=window.innerWidth;
 function Slider() {
     const [movieList,setMovieList]= useState([]);
+    const elementRef=useRef();
     useEffect(()=>{
         getTrendingMovies()
     },[])
@@ -14,11 +17,24 @@ function Slider() {
             setMovieList(resp.data.results)
         })
     }
+
+    const sliderRight=(element)=>{
+        element.scrollLeft+=screenWidth
+    }
+    const sliderLeft=(element)=>{
+        element.scrollLeft-=screenWidth
+    }
   return (
-    <div className='flex overflow-x-auto w-full px-16 py-4'>
-        {movieList.map((item,index)=>(
-            <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md'/>
-        ))}
+    <div>
+        <div>
+            <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer" onClick={()=>sliderLeft(elementRef.current)} />
+            <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0" onClick={()=>sliderRight(elementRef.current)}/>
+        </div>
+        <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth' ref={elementRef}>
+            {movieList.map((item,index)=>(
+                <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full h-[310px] lg:h-[550px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in'/>
+            ))}
+        </div>
     </div>
   )
 }
